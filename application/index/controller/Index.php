f0302bcd7c619665b2cc868f350d82e08b1ef95a
yisuayn <1572320149@qq.com>
2024-04-07 14:35:46 +0800

    聚合页tdk更新

--- application/index/controller/Index.php
+++ application/index/controller/Index.php
@@ -12,6 +12,9 @@ class Index extends Controller
         $result = Db::table('UserInfo')->select();
         $this->assign([
             'result' => $result,
+            'title'=>"晟玥的个人学习项目",
+            'keyword'=>'晟玥,TP5,thinkphp5门户网站,门户网站',
+            'descrition' =>"thinkphp5项目实战学习(主要以搭建企业门户网站+网站后台管理+sql server数据库),网站前端使用layui和bootstrap5为前端ui框架."
         ]);
         return $this->fetch('');
     }
